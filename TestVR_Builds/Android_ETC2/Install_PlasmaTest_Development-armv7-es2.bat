de36e7d06cd7b48691155c44fb00e33ee15a1cf7
fsanin <30347678+fsanin@users.noreply.github.com>
2017-08-19 19:38:15 -0700

    UploadEverything

---
+++ TestVR_Builds/Android_ETC2/Install_PlasmaTest_Development-armv7-es2.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c6f57a8354b101cbfeee597fa6c6740b5d88fa115ba5bda999ad98d9e9bf6c26
+size 1887
---
+++ TestVR_Builds/Android_ETC2/Uninstall_PlasmaTest_Development-armv7-es2.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:241eed612e1724d3534453097b5883a0470d8aa3e1caa16f2224f353a315f7cc
+size 784
